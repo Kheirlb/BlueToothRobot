7e6845b057051556a708e3bd60eaa891889c1687
Kheirlb <kheirlbparks@gmail.com>
2017-12-12 03:24:22 -0800

    importing sockets

--- socketServer_v1.py
+++ socketServer_v1.py
@@ -2,17 +2,25 @@ import sys
 import socket
 from PyQt5 import QtWidgets, QtGui
 
-#socket client info
-serverMACAddress = '00:1f:e1:dd:08:3d'
-port = 3
+#socket server info
+hostMACAddress = '00:1f:e1:dd:08:3d' # The MAC address of a Bluetooth adapter on the server. The server might have multiple Bluetooth adapters.
+port = 3 # 3 is an arbitrary choice. However, it must match the port used by the client.
+backlog = 1
+size = 1024
 s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
-s.connect((serverMACAddress,port))
-while 1:
-    text = input()
-    if text == "quit":
-        break
-    s.send(bytes(text, 'UTF-8'))
-s.close()
+s.bind((hostMACAddress,port))
+s.listen(backlog)
+try:
+    client, address = s.accept()
+    while 1:
+        data = client.recv(size)
+        if data:
+            print(data)
+            client.send(data)
+except:
+    print("Closing socket")
+    client.close()
+    s.close()
 
 #app info
 def window():
